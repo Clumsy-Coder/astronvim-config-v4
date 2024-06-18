58d3c638082d49ca8a3b658625d4914299870fbb
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-06-18 10:17:32 -0600

    build(plugin:notify): add config for `notify`

---
+++ lua/plugins/nvim-notify.lua
@@ -0,0 +1,12 @@
+-- A fancy, configurable, notification manager for NeoVim
+-- https://github.com/rcarriga/nvim-notify
+--
+---@type LazySpec
+return {
+  "rcarriga/nvim-notify",
+  opts = {
+    timeout = 3000,
+    -- opacity = 0,
+    -- background_colour = "#000000",
+  },
+}
