8528c3001caeb7cb5f9a110c0f508977b99d8cec
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-06-18 09:11:29 -0600

    build(plugin): add plugin `wakatime/vim-wakatime`

---
+++ lua/plugins/waka-time.lua
@@ -0,0 +1,8 @@
+-- automatic time tracking and metrics generated from your programming activity
+-- https://github.com/wakatime/vim-wakatime
+--
+---@type LazySpec
+return {
+  "wakatime/vim-wakatime",
+  event = "User AstroFile",
+}
