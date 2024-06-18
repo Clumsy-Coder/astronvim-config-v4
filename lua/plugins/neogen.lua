a30e9a31c0c5de5b6abc3b99d108118b3b3603f2
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-06-18 10:15:06 -0600

    build(plugin): add plugin `danymat/neogen`

---
+++ lua/plugins/neogen.lua
@@ -0,0 +1,34 @@
+-- a better Annotation/docs generator
+-- https://github.com/danymat/neogen
+-- obtained from
+--    https://code.mehalter.com/AstroNvim_user/~files/c8d6a0/lua/plugins/neogen.lua
+
+local prefix = "<Leader>a"
+---@type LazySpec
+return {
+  "danymat/neogen",
+  cmd = "Neogen",
+  dependencies = {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        n = {
+          [prefix] = { desc = "󰏫 Annotate" },
+          [prefix .. "<CR>"] = { function() require("neogen").generate() end, desc = "Current" },
+          [prefix .. "c"] = { function() require("neogen").generate({ type = "class" }) end, desc = "Class" },
+          [prefix .. "f"] = { function() require("neogen").generate({ type = "func" }) end, desc = "Function" },
+          [prefix .. "t"] = { function() require("neogen").generate({ type = "type" }) end, desc = "Type" },
+          [prefix .. "F"] = { function() require("neogen").generate({ type = "file" }) end, desc = "File" },
+        },
+      },
+    },
+  },
+  opts = {
+    snippet_engine = "luasnip",
+    languages = {
+      lua = { template = { annotation_convention = "emmylua" } },
+      typescript = { template = { annotation_convention = "tsdoc" } },
+      typescriptreact = { template = { annotation_convention = "tsdoc" } },
+    },
+  },
+}
