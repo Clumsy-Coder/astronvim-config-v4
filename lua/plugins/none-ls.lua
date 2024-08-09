e29ea862d147f4ec528e8e5078f45c6e5f9f6de0
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-08-09 17:41:31 -0600

    build(plugin:none-ls): use `codespell` ONLY for diagnostics
      ## what
      - use `codespell` ONLY for diagnostics
    
      ## how
      - check
        - https://github.com/codespell-project/codespell/issues/66#issuecomment-270019597
    
      ## why
      - by default the formatter is also enabled, sometimes overwriting
        words that misspelled in code
    
      ## where
      - ./lua/plugins/none-ls.lua
    
      ## usage
    
      ## commit id, issue or pull request

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -10,7 +10,7 @@ return {
     -- config variable is the default configuration table for the setup function call
     local null_ls = require("null-ls")
     -- local completion = null_ls.builtins.completion
-    -- local diagnostics = null_ls.builtins.diagnostics
+    local diagnostics = null_ls.builtins.diagnostics
     -- local formatting = null_ls.builtins.formatting
     -- local hover = null_ls.builtins.hover
     local code_actions = null_ls.builtins.code_actions
@@ -35,7 +35,15 @@ return {
         -- diagnostics
         -- https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md#diagnostics
         -- diagnostics.actionlint,           -- Github actions
-        -- diagnostics.codespell,            -- spell check
+        diagnostics.codespell.with({         -- spell check environments
+          extra_args = function()
+            return {
+              -- obtained from
+              -- - https://github.com/codespell-project/codespell/issues/66#issuecomment-270019597
+              "--exclude-file=$HOME/.config/nvim/spell/en.utf-8.add"
+            }
+          end
+        }),
         -- diagnostics.commitlint,           -- checks commit messages meets conventional commit format
         -- diagnostics.cspell,               -- spell checker for code
         -- diagnostics.editorconfig_checker, -- check files are using .editorconfig configs
